b1fb0585015d90d035b5d0c8d36634056ef7d21e
Aliaksandr91 <horbachalex91@gmail.com>
2023-06-10 20:37:37 +0200

    feat: add solution Rest Parameter with Function Parameters

--- freecodecamp/freecodecamp.js
+++ freecodecamp/freecodecamp.js
@@ -79,3 +79,13 @@ function getNine() {
       console.log("Still going!");
     }
   }
+
+
+//Use the Rest Parameter with Function Parameters
+  const sum = (...args) => {
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+      total += args[i];
+    }
+    return total;
+  }
