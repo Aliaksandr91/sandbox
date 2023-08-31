d8f50c358f6612e62a660f5b839fee39edcf0245
Aliaksandr91 <horbachalex91@gmail.com>
2023-08-31 20:11:51 +0200

    feat: Is it even?

---
+++ codewars/8kyu/Is it even.js
@@ -0,0 +1,9 @@
+function testEven(n) {
+    return n % 2 === 0
+}
+
+console.log(testEven(0))  //true
+console.log(testEven(0.5))  //false
+console.log(testEven(1))  //false
+console.log(testEven(2))  //true
+console.log(testEven(-4))  //true
