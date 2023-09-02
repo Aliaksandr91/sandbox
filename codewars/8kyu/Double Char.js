c13bfa8ee6366b02169c56150d3ff0dbd7be7623
Aliaksandr91 <horbachalex91@gmail.com>
2023-09-02 18:29:03 +0200

    feat: Double Char solve

---
+++ codewars/8kyu/Double Char.js
@@ -0,0 +1,9 @@
+function doubleChar(str) {
+    return str.split('').map(el=>el.repeat(2)).join('')
+}
+console.log(doubleChar("abcd"))  // "aabbccdd"
+console.log(doubleChar("Adidas"))  // "AAddiiddaass"
+console.log(doubleChar("1337"))  // "11333377"
+console.log(doubleChar("illuminati"))  // "iilllluummiinnaattii"
+console.log(doubleChar("123456"))  // "112233445566"
+console.log(doubleChar("%^&*("))  // "%%^^&&**(("
