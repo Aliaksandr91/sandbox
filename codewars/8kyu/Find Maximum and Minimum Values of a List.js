d7fc61452d02c292491c4ee5847db64042f97f87
Aliaksandr91 <horbachalex91@gmail.com>
2023-09-11 18:10:46 +0200

    feat: Find Maximum and Minimum Values of a List solve

---
+++ codewars/8kyu/Find Maximum and Minimum Values of a List.js
@@ -0,0 +1,13 @@
+var min = function(list){
+
+    return list.sort((a,b)=>a-b)[0]
+}
+
+var max = function(list){
+
+    return list.sort((a,b)=>b-a)[0]
+}
+console.log(min([-52, 56, 30, 29, -54, 0, -110]))  //-110
+console.log(min([42, 54, 65, 87, 0]))  //0
+console.log(max([4,6,2,1,9,63,-134,566]))  //566
+console.log(max([5]))  //5
