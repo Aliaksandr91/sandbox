4f55d57b66dd513520563539a455b2798d93bb1f
Aliaksandr91 <horbachalex91@gmail.com>
2023-10-02 17:54:48 +0200

    feat: Beginner Series #4 Cockroach solve

---
+++ codewars/8kyu/Beginner Series 4 Cockroach.js
@@ -0,0 +1,7 @@
+function cockroachSpeed(s) {
+    return Math.floor(s * 100000 / 3600)
+}
+
+console.log(cockroachSpeed(1.08)) // 30
+console.log(cockroachSpeed(1.09)) // 30
+console.log(cockroachSpeed(0)) // 0
