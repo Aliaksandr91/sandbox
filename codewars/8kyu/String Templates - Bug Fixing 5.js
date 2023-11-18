d6ef00aaf19424e3e99dc6fc1558f1d330f28a15
Aliaksandr91 <horbachalex91@gmail.com>
2023-11-18 17:13:01 +0100

    feat: String Templates - Bug Fixing #5 solve

---
+++ codewars/8kyu/String Templates - Bug Fixing 5.js
@@ -0,0 +1,6 @@
+function buildString(...template){
+    return `I like ${template.join(', ')}!`;
+}
+console.log(buildString('Cheese','Milk','Chocolate'))// 'I like Cheese, Milk, Chocolate!'
+console.log(buildString('Cheese','Milk'))// 'I like Cheese, Milk!'
+console.log(buildString('Chocolate'))// 'I like Chocolate!'
