e5325f1818a340f0bfba01b911e696ac5c9ab09a
Aliaksandr91 <horbachalex91@gmail.com>
2023-12-08 19:14:18 +0100

    feat: Sum of differences in array solve

---
+++ codewars/8kyu/Sum of differences in array.js
@@ -0,0 +1,14 @@
+function sumOfDifferences(arr) {
+    if (arr.length <= 1) {
+        return 0;
+    }
+    arr.sort((a, b) => b - a);
+    let sum = 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+        sum += arr[i] - arr[i + 1];
+    }
+    return sum;
+}
+
+console.log(sumOfDifferences([1, 2, 10]))// 9
+console.log(sumOfDifferences([-3, -2, -1]))// 2
