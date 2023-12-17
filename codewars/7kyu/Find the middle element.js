be52872ddb468aed638f1fc2f8ee33efa0485590
Aliaksandr91 <horbachalex91@gmail.com>
2023-12-17 13:02:11 +0100

    feat: Find the middle element solve

---
+++ codewars/7kyu/Find the middle element.js
@@ -0,0 +1,19 @@
+function gimme(triplet) {
+    let temp = [...triplet].sort((a, b) => a - b)
+    return triplet.indexOf(temp[1])
+}
+
+console.log(gimme([2, 3, 1]))  // 0
+console.log(gimme([5, 10, 14]))  // 1
+
+
+console.log(gimme([2.1, 3.2, 1.4]))  // 0
+console.log(gimme([5.9, 10.4, 14.2]))  // 1
+
+
+console.log(gimme([-2, -3, -1]))  // 0
+console.log(gimme([-5, -10, -14]))  // 1
+
+
+console.log(gimme([-2, -3.2, 1]))  // 0
+console.log(gimme([-5.2, -10.6, 14]))  // 0
