79a96d0ca6b1f1f15698131ddbab6462074995b1
Aliaksandr Horbach <135595904+Aliaksandr91@users.noreply.github.com>
2024-01-01 16:58:34 +0100

    feat: Is every value in the array an array solve

---
+++ codewars/7kyu/Is every value in the array an array.js
@@ -0,0 +1,15 @@
+const arrCheck = value => {
+    if (!Array.isArray(value)) {
+        return false;
+    }
+    return value.every(subArray => Array.isArray(subArray));
+}
+
+
+
+
+console.log(arrCheck([]))// true
+console.log(arrCheck([['string']]))// true
+console.log(arrCheck([[],{}]))// false
+console.log(arrCheck([[1],[2],[3]]))// true
+console.log(arrCheck(['A', 'R', 'R', 'A', 'Y']))// false
