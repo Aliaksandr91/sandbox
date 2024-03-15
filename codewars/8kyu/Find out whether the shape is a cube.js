a1e55235036f8ce8e48982b90c42665f8506e0a6
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-03-15 19:33:34 +0100

    Find out whether the shape is a cube solve

---
+++ codewars/8kyu/Find out whether the shape is a cube.js
@@ -0,0 +1,10 @@
+function cubeChecker(volume, side){
+    return volume > 0 && side > 0 && (Math.pow(side, 3) === volume);
+
+}
+console.log(cubeChecker(  8, 3)) // false
+console.log(cubeChecker(  8, 2)) // true
+console.log(cubeChecker( -8,-2)) // false
+console.log(cubeChecker(  0, 0)) // false
+console.log(cubeChecker(  1, 5)) // false
+console.log(cubeChecker(125, 5)) // true
--- codewars/8kyu/howMuchILoveYou.js
+++ codewars/8kyu/howMuchILoveYou.js
@@ -1,6 +1,6 @@
 function howMuchILoveYou(nbPetals) {
     const phrases = ["I love you", "a little", "a lot", "passionately", "madly", "not at all"];
-    const index = (nbPetals - 1) % phrases.length; 
+    const index = (nbPetals - 1) % phrases.length;
     return phrases[index];
 }
 console.log(howMuchILoveYou(7))//"I love you"
