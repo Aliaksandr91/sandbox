5940fc7745151c88a9a7f85613ab27276ac50184
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-04-18 18:50:37 +0200

    feat: Ensure question solve

---
+++ codewars/8kyu/Ensure question.js
@@ -0,0 +1,9 @@
+function ensureQuestion(s) {
+
+    return s[s.length - 1] === '?' ? s : s + '?';
+}
+
+console.log(ensureQuestion("")) //"?"
+console.log(ensureQuestion("Yes")) //"Yes?"
+console.log(ensureQuestion("No?")) // "No?"
+
---
+++ codewars/8kyu/Shifty Closures.js
@@ -0,0 +1,11 @@
+var name;
+
+function greetAbe() {
+    name = 'Abe'
+    return 'Hello, ' + name + '!'
+}
+
+function greetBen() {
+    name = 'Ben'
+    return 'Hello, ' + name + '!'
+}
