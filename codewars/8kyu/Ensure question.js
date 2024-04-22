19808a81ebc9cec5ffd6f198ef8d7ac17450acca
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-04-22 23:01:45 +0200

    feat: Tip Calculator solve

--- codewars/8kyu/Ensure question.js
+++ codewars/8kyu/Ensure question.js
@@ -1,5 +1,4 @@
 function ensureQuestion(s) {
-
     return s[s.length - 1] === '?' ? s : s + '?';
 }
 
---
+++ codewars/8kyu/Tip Calculator.js
@@ -0,0 +1,28 @@
+function calculateTip(amount, rating) {
+    const serviceRating = rating.toLowerCase();
+
+    let tipPercentage;
+
+    switch (serviceRating) {
+        case 'terrible':
+            tipPercentage = 0;
+            break;
+        case 'poor':
+            tipPercentage = 0.05;
+            break;
+        case 'good':
+            tipPercentage = 0.10;
+            break;
+        case 'great':
+            tipPercentage = 0.15;
+            break;
+        case 'excellent':
+            tipPercentage = 0.20;
+            break;
+        default:
+            return 'Rating not recognised';  // If rating is not recognised
+    }
+    return Math.round(amount * tipPercentage)
+}
+console.log(calculateTip(20, "Excellent"))// 4
+console.log(calculateTip(26.95, "good"))// 3
