e6784f90ec0829dbe3461a0a7605ceb9decee27a
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-05-07 23:22:50 +0200

    feat: Simple Fun #179: Fraction solve

---
+++ codewars/7kyu/Simple Fun 179 Fraction.js
@@ -0,0 +1,21 @@
+function fraction(a, b) {
+    let gcd = 1;
+    for(let i = 1; i <= a; ++i){
+        if(a % i === 0 && b % i === 0){
+            gcd = i
+        }
+    }
+    return a/ gcd + b/gcd
+
+}
+console.log(fraction(90, 120)) // 7
+
+console.log(fraction(2, 4)) // 3
+
+console.log(fraction(100, 1000)) // 11
+
+console.log(fraction(3, 15)) // 6
+
+console.log(fraction(114, 200)) // 157
+
+console.log(fraction(3, 118)) // 121
