9a262a736df7e066ce4a4dc24fa54c00908ff721
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-05-13 17:43:46 +0200

    feat: Check the exam solve

---
+++ codewars/7kyu/Check the exam.js
@@ -0,0 +1,34 @@
+function checkExam(array1, array2) {
+    let res = 0
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] === "") {
+            res += 0;
+        }
+        else if (array1[i] === array2[i]) {
+            res = res + 4
+        } else if (array1[i] !== array2[i]) {
+            res = -1
+        }
+    }
+    return res
+}
+
+function checkExam(array1, array2) {
+    let score = 0;
+    for (let i = 0; i < array1.length; i++) {
+        if (array2[i] === "") {
+            score += 0;
+        } else if (array1[i] === array2[i]) {
+            score += 4;
+        } else {
+            score -= 1;
+        }
+    }
+    return Math.max(score, 0);
+}
+
+
+console.log(checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]))// 6
+console.log(checkExam(["a", "a", "c", "b"], ["a", "a", "b", ""]))// 7
+console.log(checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]))// 16
+console.log(checkExam(["b", "c", "b", "a"], ["", "a", "a", "c"]))// 0
