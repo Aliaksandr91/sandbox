03ba71d4acad29d4dc70dbac507cc4cf099c94ab
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-05-15 16:22:38 +0200

    feat:Convert Hash To An Array solve

---
+++ codewars/7kyu/Convert Hash To An Array.js
@@ -0,0 +1,10 @@
+function convertHashToArray(hash){
+    return Object.keys(hash).map((key) => [key, hash[key]]);
+
+
+}
+console.log(convertHashToArray({name: "Jeremy"})) //[["name", "Jeremy"]]
+console.log(convertHashToArray({name: "Jeremy", age: 24}).sort()) //[["age", 24], ["name", "Jeremy"]]
+console.log(convertHashToArray({name: "Jeremy", age: 24, role: "Software Engineer"}).sort()) // //[["age", 24], ["name", "Jeremy"], ["role", "Software Engineer"]]
+console.log(convertHashToArray({product: "CodeWars", powerLevelOver: 9000}).sort()) //[["powerLevelOver", 9000], ["product", "CodeWars"]]
+console.log(convertHashToArray({})) //[]
