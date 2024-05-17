d2c9e782a0bc3b66668a5868be5ef3e8e5ab6696
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-05-17 19:12:45 +0200

    feat: Generate user links solve

---
+++ codewars/8kyu/Generate user links.js
@@ -0,0 +1,9 @@
+function generateLink(user) {
+    return `http://www.codewars.com/users/${encodeURIComponent(user)}`
+}
+
+console.log(generateLink('matt c'))  //'http://www.codewars.com/users/matt%20c'
+console.log(generateLink('g964'))  //'http://www.codewars.com/users/g964'
+console.log(generateLink('GiacomoSorbi'))  //'http://www.codewars.com/users/GiacomoSorbi'
+console.log(generateLink('ZozoFouchtra'))  //'http://www.codewars.com/users/ZozoFouchtra'
+console.log(generateLink('colbydauph'))  //'http://www.codewars.com/users/colbydauph'
