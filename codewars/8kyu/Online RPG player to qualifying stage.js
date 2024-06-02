ffcf586e8248d80d6a9c2178cacc9e86005580ba
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-06-02 12:53:07 +0200

    feat: add Multiplication table for number solve

--- codewars/8kyu/Online RPG player to qualifying stage.js
+++ codewars/8kyu/Online RPG player to qualifying stage.js
@@ -1,5 +1,5 @@
 function playerRankUp (points) {
-    return points>100 ? "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.": false
+    return points>=100 ? "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up.": false
 }
 console.log(playerRankUp(64))// false
 console.log(playerRankUp(101))// "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
