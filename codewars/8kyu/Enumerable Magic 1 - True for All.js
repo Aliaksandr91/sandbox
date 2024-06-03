45c890437823c86c95b0e9b7013451ebb67ae1aa
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-06-03 10:47:45 +0200

    feat: Enumerable Magic #1 - True for All? solve

---
+++ codewars/8kyu/Enumerable Magic 1 - True for All.js
@@ -0,0 +1,10 @@
+function all( arr, fun ){
+    for (let i = 0; i < arr.length; i++) {
+        if (!fun(arr[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+console.log(all([1,2,3,4,5], function(v){return v<9}))// true
+console.log(all([1,2,3,4,5], function(v){return v>9}))// false
