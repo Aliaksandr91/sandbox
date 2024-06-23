00cc8683fbb3a8312f885a0c7e29470847d35e86
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-06-23 15:44:30 +0200

    feat: task 26

--- freecodecamp/JavaScript Algorithms and Data Structures (Beta).js
+++ freecodecamp/JavaScript Algorithms and Data Structures (Beta).js
@@ -1,8 +1,4 @@
 let character = 'Hello';
 let count = 8;
 let rows = ["Naomi", "Quincy", "CamperChan"];
-let pushed = rows.push("freeCodeCamp");
-console.log(pushed)
-let popped = rows.pop();
-console.log(popped);
 
