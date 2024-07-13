33cc121ecececb236bd975e6dfa39f0f34a0fb72
Aliaksandr Horbach <horbachalex91@gmail.com>
2024-07-13 19:07:42 +0200

    feat: Sort the Gift Code solve

---
+++ codewars/7kyu/Sort the Gift Code.js
@@ -0,0 +1,6 @@
+function sortGiftCode(code){
+    return code.split('').sort().join('')
+}
+console.log(sortGiftCode('abcdef'))// 'abcdef'
+console.log(sortGiftCode('pqksuvy'))// 'kpqsuvy'
+console.log(sortGiftCode('zyxwvutsrqponmlkjihgfedcba'))// 'abcdefghijklmnopqrstuvwxyz'
